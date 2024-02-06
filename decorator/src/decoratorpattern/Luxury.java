f22b619689197d987bf3a94b3f8f7832c82c15ad
Héctor Cortez <updown.input@gmail.com>
2024-02-05 23:16:30 -0300

    Decorator Pattern added

---
+++ decorator/src/decoratorpattern/Luxury.java
@@ -0,0 +1,16 @@
+package decoratorpattern;
+
+abstract class Luxury extends Home {
+    protected Home home;
+
+    public double luxuryCost;
+
+    public Luxury(Home home) {
+        this.home = home;
+    }
+
+    @Override
+    public double getPrice() {
+        return home.getPrice();
+    }
+}
