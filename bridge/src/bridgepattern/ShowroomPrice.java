82a56535cfe1bc98b99c389396adbb4f8f30968a
Héctor Armando Cortez <updown.input@gmail.com>
2024-07-04 20:37:37 -0300

    Bridge Pattern display message fix

--- bridge/src/bridgepattern/ShowroomPrice.java
+++ bridge/src/bridgepattern/ShowroomPrice.java
@@ -3,6 +3,6 @@ package bridgepattern;
 public class ShowroomPrice implements PriceType {
     @Override
     public void displayProductPrice(String productType, double price) {
-        System.out.println("The " + productType + "'s online price is $" + price);
+        System.out.println("The " + productType + "'s showroom price is $" + price);
     }
 }
